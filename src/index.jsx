76b9b64b11b0faefcecbfc39bc2ab1194aed0f51
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-09-28 13:37:40 +0300

    components update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./containers/App/App";
+import App from "./components/App/App";
 
 ReactDOM.render(
   <React.StrictMode>{<App />}</React.StrictMode>,
